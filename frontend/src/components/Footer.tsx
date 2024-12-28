c0a259d90d96921cfc836aa2d3981e5dc5af1455
amaan287 <amaanmirza287@gmail.com>
2024-12-28 12:17:47 +0530

    fixing footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,35 +1,17 @@
 import { Footer } from "flowbite-react";
+import { Mail } from "lucide-react";
 // import { Link } from "react-router-dom";
-import { BsFacebook, BsInstagram, BsGithub, BsYoutube } from "react-icons/bs";
+import { BsFacebook, BsInstagram, BsYoutube, BsWhatsapp } from "react-icons/bs";
 import { Link } from "react-router-dom";
 export default function FooterCom() {
   return (
-    <div className="border w-full px-2 py-2 rounded-lg bg-background ">
+    <div className="border max-w-screen w-full px-2 py-2 rounded-lg bg-background ">
       <div className="w-full px-2 py-2 max-w-7xl mx-auto">
         <div className="grid w-full justify-between sm:flex md:grid-cols-1">
-          {/* <div className="mt-5">
-            <Link
-              to="/"
-              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
-            >
-              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-                Sahand's
-              </span>
-              Blog
-            </Link>
-          </div> */}
           <div className="grid grid-cols-2 gap-8 mt-4 py-2 sm:grid-cols-3 sm:gap-6">
             <div>
               <Footer.Title title="Follow us" className="pb-2 " />
               <Footer.LinkGroup col>
-                <Link
-                  to="https://www.github.com/amaan287"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className=""
-                >
-                  Github
-                </Link>
                 <Link
                   to="https://www.facebook.com/priyansh.soni.12576"
                   target="_blank"
@@ -48,6 +30,28 @@ export default function FooterCom() {
                 >
                   Youtube
                 </Link>
+              </Footer.LinkGroup>
+            </div>
+
+            <div>
+              <Footer.Title title="Contact Us" className="pb-2" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSKjgCQbcxnCJLLJlkzDlbpNPMKMrbCckKbXlVSmKrhDkFnlkhVFTQtSTXdpJSSjxHRdWnWS"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Mail
+                </Footer.Link>
+                <Footer.Link
+                  href={`https://wa.me/${"+919784404469"}?text=${encodeURIComponent(
+                    `Hello, I am interested in your services. Please provide me with more details.`
+                  )}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  WhatsApp
+                </Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
@@ -73,18 +77,32 @@ export default function FooterCom() {
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon
+              target="_blank"
               href="https://www.facebook.com/priyansh.soni.12576"
               icon={BsFacebook}
             />
             <Footer.Icon
+              target="_blank"
               href="https://www.instagram.com/priyanshsoniii/"
               icon={BsInstagram}
             />
             <Footer.Icon
+              target="_blank"
               href="https://youtube.com/@priyanshsoniii?si=ixnJZJAczAKWnYgi"
               icon={BsYoutube}
             />
-            <Footer.Icon href={"https://github.com/amaan287"} icon={BsGithub} />
+            <Footer.Icon
+              target="_blank"
+              href={`https://wa.me/${"+919784404469"}?text=${encodeURIComponent(
+                `Hello, I am interested in your services. Please provide me with more details.`
+              )}`}
+              icon={BsWhatsapp}
+            />
+            <Footer.Icon
+              target="_blank"
+              href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSKjgCQbcxnCJLLJlkzDlbpNPMKMrbCckKbXlVSmKrhDkFnlkhVFTQtSTXdpJSSjxHRdWnWS"
+              icon={Mail}
+            />
           </div>
         </div>
       </div>
